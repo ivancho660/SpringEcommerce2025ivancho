4c2aa8bfdc773e848258c15026702c7c7b7c6fb5
ivanchomelo <ichernandez47@soy.sena.edu.co>
2024-10-18 14:32:49 -0500

    v0.5_CreacionInterfacesService_18102024_ivancho

---
+++ src/main/java/com/sena/ecommerce/service/IOrdenService.java
@@ -0,0 +1,20 @@
+package com.sena.ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.sena.ecommerce.model.Orden;
+import com.sena.ecommerce.model.Usuario;
+
+public interface IOrdenService {
+
+	public Orden save(Orden orden);
+
+	public List<Orden> findAll();
+
+	public String GenerarNumeroOrden();
+
+	public List<Orden> findByUsuario(Usuario usuario);
+
+	public Optional<Orden> findById(Integer id);
+}
---
+++ src/main/java/com/sena/ecommerce/service/IUsuarioService.java
@@ -0,0 +1,23 @@
+package com.sena.ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.sena.ecommerce.model.Usuario;
+
+public interface IUsuarioService {
+
+	public Usuario save(Usuario usuario);
+
+	public Optional<Usuario> get(Integer id);
+
+	public void update(Usuario usuario);
+
+	public void delete(Integer id);
+
+	public Optional<Usuario> findById(Integer id);
+
+	public Optional<Usuario> findById(String email);
+
+	List<Usuario> findAll();
+}
